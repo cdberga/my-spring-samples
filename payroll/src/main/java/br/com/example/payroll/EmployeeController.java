20f1cf1c706470c661a22c293404501000b08630
Carlos <carlos.bergamasco@gmail.com>
2020-05-08 12:25:02 -0300

    Added Resource Assembler to simplify controller Get mappings

--- payroll/src/main/java/br/com/example/payroll/EmployeeController.java
+++ payroll/src/main/java/br/com/example/payroll/EmployeeController.java
@@ -19,9 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 
 	private final EmployeeRepository repository;
+	
+	private final EmployeeModelAssembler assembler; 
 
-	EmployeeController(EmployeeRepository repository) {
+	EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
 		this.repository = repository;
+		this.assembler = assembler;
 	}
 
 //	Aggregate root
@@ -50,9 +53,7 @@ public class EmployeeController {
 		Employee employee = repository.findById(id)
 				.orElseThrow(() -> new EmployeeNotFoundException(id));
 		
-		return new EntityModel<> (employee,
-				linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-				linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+		return assembler.toModel(employee);
 	}
 
 	@PutMapping("/employees/{id}")
