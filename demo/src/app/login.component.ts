152a33b1e6e587500f87e320e59e31c366242455
Carlos <carlos.bergamasco@gmail.com>
2020-05-18 18:17:07 -0300

    Adjusted Angular paths and imports. Added interceptor

--- demo/src/app/login.component.ts
+++ demo/src/app/login.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {AppService} from './app/service';
+import {AppService} from './app.service';
 import {HttpClient} from '@angular/common/http';
 import {Router} from '@angular/router';
 
 @Component({
-	templateUrl: './login/component.html'
+	templateUrl: './login.component.html'
 })
 export class LoginComponent{
 
@@ -13,7 +13,7 @@ export class LoginComponent{
 	constructor(private app: AppService, private http: HttpClient, private router: Router){}
 	
 	login(){
-		this.app.authenticate(this.credentials, {} => {
+		this.app.authenticate(this.credentials,() => {
 			this.router.navigateByUrl('/');
 		});
 		return false;
